8f9303c7bb9aea099e13363286626ab17882e9fb
eswaribala <parameswaribala@gmail.com>
2020-12-02 15:58:16 +0530

    Store Front v1

---
+++ storefrontwebapp/src/main/java/com/lti/storefront/StorefrontwebappApplication.java
@@ -0,0 +1,13 @@
+package com.lti.storefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorefrontwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorefrontwebappApplication.class, args);
+	}
+
+}
---
+++ storefrontwebapp/src/test/java/com/lti/storefront/StorefrontwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefrontwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
