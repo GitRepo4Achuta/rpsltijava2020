c379aa31fe947d4d5768140b1d26b61de5a81223
eswaribala <parameswaribala@gmail.com>
2020-12-03 18:04:02 +0530

    Inv service Eureka

--- inventoryservice/src/main/java/com/lti/inventory/InventoryserviceApplication.java
+++ inventoryservice/src/main/java/com/lti/inventory/InventoryserviceApplication.java
@@ -2,8 +2,10 @@ package com.lti.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryserviceApplication {
 
 	public static void main(String[] args) {
